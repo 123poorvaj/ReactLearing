1f5528385fb1f8f0f2d7992f7fccaf8107b6d701
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-05-18 19:03:00 +0530

    Add files via upload

---
+++ day3/index.jsx
@@ -0,0 +1,15 @@
+function Userprofil(){
+    return(
+        <>
+        <div className="profilesContainer">
+            <img src="https://tse3.mm.bing.net/th/id/OIP.t8LAKbP7baO4yx90pdyfZAHaJt?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="pimge" className="pimage"></img>
+            <div className="profilesContainer_details">
+                <h1>name</h1>
+                <p>role</p>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Userprofil;
